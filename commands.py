0dcb83792c07d5284e659534e15de789c7eedb78
NANA-PREMPEH <52207181+NANA-PREMPEH@users.noreply.github.com>
2021-10-05 13:59:19 +0000

    create_table

--- commands.py
+++ commands.py
@@ -1,4 +1,9 @@
 from flaskblog import db
+import click
+from flask.cli import with_appcontext
 
 
-db.create_all()
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
