59a8a2fa825ac1da75975ee1ffc5a41d4536f35f
unknown <HP@DESKTOP-PREMPEH.highways.ho.gh>
2021-12-11 03:38:59 +0000

    updated database url1

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,6 +4,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # new for heroku database config.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
