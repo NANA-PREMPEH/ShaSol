e795d1895b4858b41134338b473310598e08bd82
unknown <HP@DESKTOP-PREMPEH.highways.ho.gh>
2021-12-14 17:53:02 +0000

    new venv

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,12 @@
 import os
 import re
+import psycopg2
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # new for heroku database config.
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_old')
+    # converting database url from postgres --> postgresql
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
