c5695c344aa380af208c769a5422684d42601f31
unknown <HP@DESKTOP-PREMPEH.highways.ho.gh>
2021-12-15 01:32:39 +0000

    new Config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # new for heroku database config.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     # converting database url from postgres --> postgresql
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
